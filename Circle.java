58af0744ed7454540ef04bc4388a2dbae44d69b2
KritikaChoudhary <43022769+KritikaChoudhary@users.noreply.github.com>
2018-12-09 21:57:17 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.lang.Math;
+class Circle {
+	Scanner sc=new Scanner(System.in);
+	int radius;
+	Circle()
+	{
+		
+	}
+	Circle(int radius) {
+		// TODO Auto-generated constructor stub
+		this.radius=radius;
+	}
+	double area()
+	{
+		System.out.print("Area of circle");
+		return Math.PI*radius*radius;
+	}
+}
+class Sector extends Circle
+{
+	float angle;
+	Sector()
+	{
+		
+	}
+	Sector(float angle) {
+		// TODO Auto-generated constructor stub
+		this.angle=angle;
+	}
+	double area()
+	{
+		System.out.print("Area of sector");
+		return (angle*radius*radius)/2;
+	}
+	
+}
+class Segment extends Circle
+{
+	float length;
+	Segment()
+	{
+		
+	}
+	Segment(float length) {
+		// TODO Auto-generated constructor st
+		this.length=length;
+	}
+	double area()
+	{
+		System.out.print("Area of segment");
+		double h=radius-Math.pow((Math.pow(radius,2)-Math.pow(length, 2)),0.5);
+		return (((2*h*length)/3)+((h*h*h)/(2*length)));
+	}
+}
+class Main
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int ch;
+		do
+		{
+			System.out.println("\nArea of\n1.Circle\n2.Segment\n3.Sector\n4.Exit\nEnter choice:");
+			ch=sc.nextInt();
+			switch(ch)
+			{
+			case 1:System.out.print("Enter the radius of the circle:");
+					int r=sc.nextInt();
+					Circle c=new Circle(r);
+					System.out.print(c.area());
+					break;
+			case 2:	System.out.print("\nEnter the length of the segment:");
+					float f=sc.nextFloat();
+					Segment s=new Segment(f);
+					System.out.print(s.area());
+					break;
+			case 3:System.out.print("\nEnter the angle of the segment:");
+					float a=sc.nextFloat();
+					Sector p=new Sector(a);
+					System.out.print(p.area());
+			}
+		
+		}while(ch!=4);
+		
+	}
+}
